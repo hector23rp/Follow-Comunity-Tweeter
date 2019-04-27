eb74750e858d2156d8586aa5707280f1a6afd8f3
hector23rp <hector23rp@gmail.com>
2019-04-27 12:12:12 +0200

    SSH & connect to Mongo

--- index.ts
+++ index.ts
@@ -4,12 +4,16 @@ import { Twit } from './models/twit';
 import { config } from './config';
 import * as Twitterstream from 'node-tweet-stream';
 import { secret } from './secret';
+import * as tunnel from 'tunnel-ssh';
+let fs = require('fs');
 
 class App {
   
   private client: Twitter;
   private uriMongo: string = config.uriMongo;
   private client_stream: Twitterstream;
+  private ssh_client: node_ssh;
+  private serverTunnel;
 
   initialize() {
     // Creación de cliente de Twitter
@@ -24,10 +28,19 @@ class App {
     // Conexión a MongoDB
     console.log('Connecting with Mongo...');
     return new Promise((resolve, rejected) => {
-      mongoose.connect(this.uriMongo, (error) => {
-        if (error) return rejected(error);
-
-        return resolve(true);
+      this.serverTunnel = tunnel({
+        host: '18.233.5.144',
+        username: 'ubuntu',
+        privateKey: fs.readFileSync('<path_key>'),
+        port: 22,
+        dstPort: 27017
+      }, function(error, server) {
+        console.log('Conexión SSH realizada correctamente!');
+        mongoose.connect(config.uriMongo, (error) => {
+          if (error) return rejected(error);
+  
+          return resolve(true);
+        });
       });
     });
   }
@@ -49,7 +62,10 @@ const app = new App();
 app.initialize().then(response => {
   console.log('Connected with MongoDB!');
   console.log('Searhcing tweets....');
-  app.run();
+  Twit.count({}, function(err, count) {
+    console.log('Number of tweets: ',count);
+  });
+  // app.run();
 }).catch(err => {
   console.error(err);
 })
